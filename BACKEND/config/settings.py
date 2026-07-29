30b82800117cd4ab2521d48fc2fd98ea95cf7ee5
Abdellatif Hissoune <haissouneabdellaif749@gmail.com>
2026-07-30 00:56:49 +0100

    here I add the connection between pgadmin and my django app

--- BACKEND/config/settings.py
+++ BACKEND/config/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ai_learning_platform',
+        'USER': 'postgres',
+        'PASSWORD': 'cvcv2005',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
