db245c56012e7c6aed736d5710dc65e4b397979f
Abdellatif Hissoune <haissouneabdellaif749@gmail.com>
2026-07-30 02:03:53 +0100

    Implement PDF upload API

--- BACKEND/config/settings.py
+++ BACKEND/config/settings.py
@@ -136,3 +136,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- BACKEND/documents/views.py
+++ BACKEND/documents/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import DocumentSerializer
+
+
+class DocumentUploadView(APIView):
+
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request):
+        serializer = DocumentSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            uploaded_file = request.FILES["file"]
+
+            title = os.path.splitext(uploaded_file.name)[0]
+
+            serializer.save(
+                owner=request.user,
+                title=title
+            )
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
