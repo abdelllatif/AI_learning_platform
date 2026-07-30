2e4bd08f8d24e99c1dba037060476452b695df42
Abdellatif Hissoune <haissouneabdellaif749@gmail.com>
2026-07-30 01:41:06 +0100

    Add Document model

--- BACKEND/documents/models.py
+++ BACKEND/documents/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Document(models.Model):
+
+    title = models.CharField(max_length=255)
+
+    file = models.FileField(upload_to="documents/")
+
+    language = models.CharField(
+        max_length=50,
+        blank=True
+    )
+
+    status = models.CharField(
+        max_length=20,
+        default="UPLOADED"
+    )
+
+    uploaded_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="documents"
+    )
+
+    def __str__(self):
+        return self.title
